84c3d5d4b00788d80fbb80f66e84eb22539a6d0e
Alex Gilmer <alex.gilmer.business@gmail.com>
2024-07-04 15:33:20 -0500

    Add project files.

---
+++ 2023-day-01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _2023_day_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static IList<string> GetPuzzleInput()
+        {
+
+        }
+    }
+}
